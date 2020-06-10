e5603c14da037d374f5754b733386927385ae311
irlcaf <carlomagnoaf@hotmail.com>
2020-06-10 14:01:28 +0800

    Testing division with periodic data frame

--- ECU_.py
+++ ECU_.py
@@ -21,6 +21,7 @@ channel = 'vcan0'
 mt_sending = MerkleTools()
 mt_receiving = MerkleTools()
 
+
 def xor(var, key):
     """
         - ** var ** *bit_string* : First input of the xor operation
@@ -52,16 +53,15 @@ def encryption(current_anchor_random_number, message, can_id_key, can_id_counter
         backend = default_backend()
     )
     kdf_output = kdf.derive(can_id_key)
-    #rint("Encryption derivation key:", kdf_output)
     hash_digest = sha256(kdf_output).hexdigest().encode()
 
     length = len(message)#+len(can_id_counter)
     hash_digest = hash_digest[:length]
 
-    data_frame = message #+ can_id_counter
+    data_frame = message
 
     ciphertext = xor(data_frame,hash_digest)
-
+   
     return ciphertext
 
 def decryption(current_anchor_random_number, ciphertext, can_id_key, can_id_counter, nonce=0):
@@ -83,69 +83,12 @@ def decryption(current_anchor_random_number, ciphertext, can_id_key, can_id_coun
         backend = default_backend()
     )
     kdf_output = kdf.derive(can_id_key)
+    #print("Decryption derivation key: ",kdf_output)
     hash_digest = sha256(kdf_output).hexdigest().encode()
     length = len(ciphertext)
     hash_digest = hash_digest[:length]
 
     data_frame = xor(ciphertext, hash_digest)
-    message = data_frame
-
-    return message
-
-def generateHanchor(current_anchor_random_number, can_id_key, can_id_counter, nonce=0):
-    """
-        Generates the anchor frame mentioned by Lin et. al
-        - ** current_anchor_random_number ** *bytes* : Anchor random number being broadcasted through the bus
-        - ** can_id_key ** *bit_string* : bit string key, length of 32, 64, 128 bits.
-        - ** can_id_counter ** *bytes* : Counter
-        - ** nonce ** *number* : Cryptographically (NO USE)
-
-        Returns the decrypted ciphertext in form of bytes.
-    """
-    salt = current_anchor_random_number
-    kdf = PBKDF2HMAC(
-        algorithm = hashes.SHA256() ,
-        length = 64,
-        salt = salt,
-        iterations = 10000,
-        backend = default_backend()
-    )
-    kdf_output = kdf.derive(can_id_key)
-    hash_digest = sha256(kdf_output).hexdigest().encode()
-
-    length = len(current_anchor_random_number)#+len(can_id_counter)
-    hash_digest = hash_digest[:length]
-
-    data_frame = current_anchor_random_number #+ can_id_counter
-
-    ciphertext = xor(data_frame,hash_digest)
-
-    return ciphertext
-
-def verifyHanchor(current_anchor_random_number, can_id_key, can_id_counter, nonce=0):
-    """
-        Verifies the anchor frame mentioned by Lin et. al
-        - ** current_anchor_random_number ** *bytes* : Anchor random number being broadcasted through the bus
-        - ** can_id_key ** *bit_string* : bit string key, length of 32, 64, 128 bits.
-        - ** can_id_counter ** *bytes* : Counter
-        - ** nonce ** *number* : Cryptographically (NO USE)
-
-        Returns the decrypted ciphertext in form of bytes.
-    """
-    salt = current_anchor_random_number
-    kdf = PBKDF2HMAC(
-        algorithm = hashes.SHA256(),
-        length = 64,
-        salt = salt,
-        iterations = 10000,
-        backend = default_backend()
-    )
-    kdf_output = kdf.derive(can_id_key)
-    hash_digest = sha256(kdf_output).hexdigest().encode()
-    length = len(current_anchor_random_number)
-    hash_digest = hash_digest[:length]
-
-    data_frame = xor(current_anchor_random_number, hash_digest)
     message = data_frame
 
     return message
@@ -172,20 +115,7 @@ def sendData(id, ciphertext):
             mt_sending.make_tree()
 
     bus.send(message)
-
-def periodicData(id, current_anchor_random_number):
-    """
-        Sends periodic data frames through the CAN bus.
-        - ** id ** *bytes* : ID key identifying the message in the bus.
-        - ** ciphertext ** *bit_string* : ciphertext being send through the CAN bus
-    """
-    bus = can.interface.Bus(channel=channel, bustype=bustype)
-
-    msg = can.Message(arbitration_id=id, data=current_anchor_random_number, is_extended_id=False)
-    task = bus.send_periodic(msg, 0.20)
-    assert isinstance(task, can.CyclicSendTaskABC)
-    task.stop()
-    time.sleep(2)
+    time.sleep(1)
 
 def randomData(id, current_anchor_random_number):
     """
@@ -202,25 +132,20 @@ def randomData(id, current_anchor_random_number):
 
             length_data = random.randint(0,8)
             random_data = get_random_bytes(length_data)
-            print("Unencrypted message from broadcast ECU: %s", random_data.hex())
+            #print("Unencrypted message from ECU_ is: %s", random_data.hex())
             ciphertext = encryption(current_anchor_random_number, random_data, can_id_key, can_id_counter)
-            #print("Encrypted message from broadcast ECU: %s", ciphertext.hex())
-            sendData(id, ciphertext)
-            time.sleep(1)
+            #ciphertext = sendData()
 
-def periodicBroadcast(id, current_anchor_random_number):
-    while(True):
-        periodicData(id, current_anchor_random_number)
-    
+            sendData(id, ciphertext)
+
 def receiveData(id, current_anchor_random_number):
     """
         Receives all the data that is going through the bus.ID is used to distinguish what messages we want to pull out.
         - ** id ** *bytes* : ID key identifying the message pulling from the bus.
     """
-    print("Im receiving data")
     bus = can.interface.Bus(channel=channel, bustype=bustype)
 
-    IDs = ["0x3aa"]
+    IDs = ["0x2aa"]
     can_id_counter = get_random_bytes(1)
     can_id_key = b'thisisjustakeythisisjustakeeyID1'
 
@@ -228,20 +153,21 @@ def receiveData(id, current_anchor_random_number):
         #Should be the periodic verified anchor number instead of the 303030303030
         if(hex(message.arbitration_id) in IDs):  
             if(message.data.hex() == "3030303030303030"):
-                #print("Merkle tree root hash, receiving: %s",mt_receiving.get_merkle_root())
+                print("Merkle tree root hash, receiving: %s",mt_receiving.get_merkle_root())
                 mt_receiving.reset_tree()
             else:
+                #print("Message from the interface: ",message.data.hex())
                 mt_receiving.add_leaf(message.data.hex())
                 mt_receiving.make_tree()  
                 message = decryption(current_anchor_random_number, message.data,can_id_key, can_id_counter)
-                #print("Decrypted message from ECU_: %s", message)
+                print("Decrypted message from broadcastECU is %s", message.hex())
     return 0
 
 try:
         current_anchor_random_number = get_random_bytes(8)
         current_anchor_random_number = b'\xfd\xf2\xcdQO\x1b\xa6\x06'
-        IDa = 0x2aa
-        IDb = 0x2ab
+        IDa = 0x3aa
+        IDb = 0x3ab
 
         thread_1 = threading.Thread(target=randomData, args=(IDa, current_anchor_random_number,))
         #thread_2 = threading.Thread(target=periodicBroadcast, args=(IDb, current_anchor_random_number,))
